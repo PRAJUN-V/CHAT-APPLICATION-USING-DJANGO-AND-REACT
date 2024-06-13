fcb4bdd002da1fb9ffa38e6d9f067a3a340da526
prajun v <prajunvelandi@gmail.com>
2024-06-13 17:03:49 +0530

    just simple frontend design completed

--- backend/webapp/urls.py
+++ backend/webapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Example URL pattern
+    path('', views.index, name='index'), 
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('chat/', views.chat, name="chat"),
 ]
